f1a038e2926778f99527e61517c13d2073fef493
VIKASH SAINI <143334443+vikash-saini-maruti@users.noreply.github.com>
2024-01-13 15:11:51 +0530

    new update

--- yt_downloader/downloader/views.py
+++ yt_downloader/downloader/views.py
@@ -1,9 +1,5 @@
-#views 3
-
-from django.http import FileResponse
 from django.shortcuts import render
 from pytube import YouTube
-import os
 
 def index(request):
     if request.method == 'POST':
@@ -11,12 +7,8 @@ def index(request):
         try:
             yt = YouTube(video_url)
             video = yt.streams.filter(file_extension='mp4', progressive=True).first()
-            video_path = f'media/{yt.title}.mp4'
-            video.download('media/', filename=yt.title)
-            
-            response = FileResponse(open(video_path, 'rb'), as_attachment=True)
-            response['Content-Disposition'] = f'attachment; filename="{yt.title}.mp4"'
-            return response
+            video.download('media/')
+            context = {'success': True, 'title': yt.title, 'filename': video.title}
         except Exception as e:
             context = {'error': True, 'error_message': str(e)}
     else:
